4f999c70268da3f8fbe2e2ee8c772f341755a81c
Mondotosz <git@moncorp.ch>
2025-08-29 09:41:30 +0200

    fixed linter warnings
    fixed linter warnings for missing defaults and updated the eslint rules
    to enforce self closing of html void elements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt();
+export default withNuxt().override("nuxt/vue/rules", {
+  rules: {
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "always",
+          component: "always",
+        },
+        svg: "always",
+        math: "always",
+      },
+    ],
+  },
+});
 // Your custom configs here
