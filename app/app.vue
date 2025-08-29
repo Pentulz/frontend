6d6d4058e0225548e5ed31473c5a4fe105034edb
Mondotosz <git@moncorp.ch>
2025-08-29 09:12:59 +0200

    included toast notification globally

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { Toaster } from "~/components/ui/sonner";
+import "vue-sonner/style.css";
+</script>
 <template>
   <NuxtRouteAnnouncer />
+  <Toaster class="pointer-events-auto" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
